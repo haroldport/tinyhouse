81ff614882b2c77319e70ad3231d39f30502ad90
Harold Portocarrero <harold.port@gmail.com>
2020-03-02 12:39:42 -0500

    Added database models and refactor seeds.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,29 +8,29 @@ import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
 const client = new ApolloClient({
-    uri: "/api",
+  uri: "/api",
 });
 
 const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/host" component={Host} />
-                <Route exact path="/listing/:id" component={Listing} />
-                <Route exact path="/listings/:location?" component={Listings} />
-                <Route exact path="/user/:id" component={User} />
-                <Route exact component={NotFound} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route exact component={NotFound} />
+      </Switch>
+    </Router>
+  );
 };
 
 render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById("root"),
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
